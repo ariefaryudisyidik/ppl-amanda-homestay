3d1fc80c985081f9d7c8d8d5444b585656503b72
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2020-11-03 13:18:09 +0700

    Add cardview in layout_main

--- app/src/main/java/com/excode/amandahomestay/data/LoginDataSource.java
+++ app/src/main/java/com/excode/amandahomestay/data/LoginDataSource.java
@@ -16,7 +16,7 @@ public class LoginDataSource {
             LoggedInUser fakeUser =
                     new LoggedInUser(
                             java.util.UUID.randomUUID().toString(),
-                            "Expiro!");
+                            "Amanda!");
             return new Result.Success<>(fakeUser);
         } catch (Exception e) {
             return new Result.Error(new IOException("Error logging in", e));
